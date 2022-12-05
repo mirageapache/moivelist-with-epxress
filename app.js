fa4d0d1e4f310cc44e4dd569f6aa4cd0885a1dd3
James <mirageapache@gmail.com>
2022-12-05 09:30:29 +0800

    Set static file

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
-app.get('/', (req,res) => {
-  res.send('this websit is for movie list')
+//setting template engine(樣版引擎)
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// setting static files(載備靜態網頁Bootstrap、pooper等js、css檔)
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(port, () => {
